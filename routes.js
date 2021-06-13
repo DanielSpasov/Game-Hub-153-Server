1463250b4b97e4af8712eebb083581389c7d6d45
ShadyGotRabies <danielspasov4@gmail.com>
2021-06-13 10:02:16 +0300

    Added Login & Register.

--- routes.js
+++ routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
 const gamesController = require('./controllers/gamesController')
+const userController = require('./controllers/userController')
 
 router.use('/', homeController)
 router.use('/games', gamesController)
+router.use('/user', userController)
 
 router.get('*', (req, res) => {
     res.json({ message: 'Page not found' })
