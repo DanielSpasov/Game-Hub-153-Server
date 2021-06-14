55de3a66bfb7cc43775c911f1179997fd76220fc
ShadyGotRabies <danielspasov4@gmail.com>
2021-06-14 09:04:47 +0300

    Made a custom message if the game name is a duplicate.

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-    res.status(400).json({ msg: err._message })
+
+    if (err.code === 11000) return res.status(400).json({ msg: 'Game with this name already exists' })
+
+    return res.status(400).json({ msg: err._message })
 }
 
 module.exports = errorHandler
